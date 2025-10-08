ff6eda66544f9b116108f88ea0927be64939b55f
Suryavhi Das <dassuryavhi704@gmail.com>
2025-10-08 23:35:24 +0530

    updating exception & setup

--- setup.py
+++ setup.py
@@ -1,26 +1,23 @@
 from setuptools import setup, find_packages
 from typing import List
 
-def get_requirements(file_path:str)->list[str]:
-
-    '''
+def get_requirements(file_path: str) -> List[str]:
+    """
     This function will return the list of requirements
-    '''
-    requirements = []
-    with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n", "") for req in requirements]
-        
-        if '-e .' in requirements:
-            requirements.remove('-e .')
+    """
+    requirements: List[str] = []
+    with open(file_path, encoding="utf-8") as file_obj:
+        requirements = [req.strip() for req in file_obj.readlines() if req.strip()]
+        if "-e ." in requirements:
+            requirements.remove("-e .")
     return requirements
 
 
 setup(
-    name='mlproject',
-    virsion='0.1.0',
-    author='Suryavhi',
-    author_email='dassuryavhi704@gmail.com',
+    name="mlproject",
+    version="0.1.0",
+    author="Suryavhi",
+    author_email="dassuryavhi704@gmail.com",
     packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+    install_requires=get_requirements("requirements.txt"),
 )
